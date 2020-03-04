1aa910b69a6894cc8052a471c54fb05fd603337c
fdezcaminero <fdezcaminero@gmail.com>
2020-03-04 12:40:31 -0400

    Created the Board and the first prompt

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,29 @@
 #!/usr/bin/env ruby
 
-puts 'Hello World'
+the_board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] 
+
+def print_board(board)
+    puts ''
+    (0..2).each do |row|
+      print '|'
+      (0..2).each do |col|
+        print board[row][col]
+        print '|'
+      end
+      puts ''
+    end
+    puts ''
+end
+
+puts print_board(the_board)
+
+puts "Type a number from 1 to 9"
+
+xOrO = gets.chomp
+
+if ('1'..'9').include? xOrO
+    puts "Cool!"
+else
+    puts xOrO
+    puts "Try again!!!"
+end
