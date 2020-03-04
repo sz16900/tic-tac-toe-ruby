bcb8e96c25ef5dfda43d62b88a3eab03a005a18a
fdezcaminero <fdezcaminero@gmail.com>
2020-03-04 13:54:07 -0400

    Big Lintin'

--- bin/main.rb
+++ bin/main.rb
@@ -1,39 +1,39 @@
 #!/usr/bin/env ruby
 
-the_board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] 
+the_board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
 def print_board(board)
-    puts ''
-    (0..2).each do |row|
+  puts ''
+  (0..2).each do |row|
+    print '|'
+    (0..2).each do |col|
+      print board[row][col]
       print '|'
-      (0..2).each do |col|
-        print board[row][col]
-        print '|'
-      end
-      puts ''
     end
     puts ''
+  end
+  puts ''
 end
 
 def user_input
-  xOrO = gets.chomp
-  if ('1'..'9').include? xOrO
-    xOrO = xOrO.to_i
-    puts "Now, your move is displayed on the board in position #{xOrO}"
+  x = gets.chomp
+  if ('1'..'9').cover? x
+    x = x.to_i
+    puts "Now, your move is displayed on the board in position #{x}"
   else
-      puts xOrO
-      puts "Try again!!!"
+    puts x
+    puts 'Try again!!!'
   end
-  xOrO
+  x
 end
 
-
 puts print_board(the_board)
-puts "Player X : Type a number from 1 to 9"
-user_input()
-puts "Player O : Type a number from 1 to 9"
-user_input()
-puts "Player X : Type a number from 1 to 9"
-user_input()
-puts "Player O : Type a number from 1 to 9"
-user_input()
+puts 'Player X : Type a number from 1 to 9'
+user_input
+puts 'Player O : Type a number from 1 to 9'
+user_input
+puts 'Player X : Type a number from 1 to 9'
+user_input
+puts 'Player O : Type a number from 1 to 9'
+user_input
+puts 'Player X Wins!'
