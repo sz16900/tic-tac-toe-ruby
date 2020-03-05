b9bd22975665dd6a001c3a34fa884695dec209f1
sz16900 <sz16900@my.bristol.ac.uk>
2020-03-05 17:02:17 -0500

    :sparkles: game logic
    introduce game logic, split class into respective files, introduce new feature for score board, infinit loop until prompted to stop

--- bin/main.rb
+++ bin/main.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/env ruby
 
-require_relative "../lib/display"
-require_relative "../lib/board"
-require_relative "../lib/game"
+require_relative '../lib/display'
+require_relative '../lib/board'
+require_relative '../lib/game'
 
 g = Game.new
 g.start
--- lib/display.rb
+++ lib/display.rb
@@ -1,49 +1,73 @@
 class Display
-    attr_reader :position
-  
-    def initialize
-      @position = nil
-    end
-  
-    def print_board(board)
-      puts ''
-      (0..2).each do |row|
+  attr_reader :position
+
+  def initialize
+    @position = nil
+  end
+
+  def print_board(board)
+    puts ''
+    (0..2).each do |row|
+      print '|'
+      (0..2).each do |col|
+        print board[row][col]
         print '|'
-        (0..2).each do |col|
-          print board[row][col]
-          print '|'
-        end
-        puts ''
       end
       puts ''
     end
-  
-    def user_input(player)
-      puts ''
-      puts "Ready Player #{player}?"
-      print 'Choose a number between 1 and 9: '
-      x = gets.chomp
-      raise 'An Error' unless ('1'..'9').include? x
-  
-      @position = x.to_i
-    rescue StandardError
-      puts ''
-      puts 'Wrong Input! Please try again.'
-      retry
-    end
-  
-    def position_taken
-      puts ''
-      puts 'Position taken! Please try again.'
-    end
-  
-    def player_wins(player)
-      puts ''
-      puts "Player #{player} is the WINNER!!!"
-    end
-  
-    def draw
-      puts ''
-      puts "It's a DRAW!!!"
-    end
+    puts ''
   end
+
+  def user_input(player)
+    puts ''
+    puts "Ready Player #{player}?"
+    print 'Choose a number between 1 and 9: '
+    x = gets.chomp
+    raise 'An Error' unless ('1'..'9').include? x
+
+    @position = x.to_i
+  rescue StandardError
+    puts ''
+    puts 'Wrong Input! Please try again.'
+    retry
+  end
+
+  def position_taken
+    puts ''
+    puts 'Position taken! Please try again.'
+  end
+
+  def player_wins(player)
+    puts ''
+    puts "Player #{player} is the WINNER!!!"
+  end
+
+  def draw
+    puts ''
+    puts "It's a DRAW!!!"
+  end
+
+  def score_board(player_hash)
+    puts 'Scores'
+    player_hash.each do |k, v|
+      puts "#{k} : #{v} "
+    end
+    puts ''
+  end
+
+  def play_again?
+    puts 'Play Again? y/n'
+    yes = gets.chomp
+    if yes == 'y'
+      false
+    elsif yes == 'n'
+      true
+    else
+      raise 'An error'
+    end
+  rescue StandardError
+    puts ''
+    puts 'Wrong Input! Please try again.'
+    retry
+  end
+end
