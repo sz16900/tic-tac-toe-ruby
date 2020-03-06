769cb314f6c64f5557af33c4fa0e1df9c3db374e
sz16900 <sz16900@my.bristol.ac.uk>
2020-03-06 14:51:19 -0500

    :construction: needs to switch player on play_again?
    same as title

--- bin/main.rb
+++ bin/main.rb
@@ -4,5 +4,62 @@ require_relative '../lib/display'
 require_relative '../lib/board'
 require_relative '../lib/game'
 
-g = Game.new
-g.start
+@display = Display.new
+@board = Board.new
+@position = nil
+@game = Game.new
+
+def win_method
+    puts @display.player_wins(@board.the_player)
+    @game.scores[@board.the_player] += 1
+    end_game_handler
+  end
+
+  def draw_method
+    puts @display.draw
+    @game.scores['draws'] += 1
+    end_game_handler
+  end
+
+
+  def end_game_handler()
+    puts @display.print_board(@board.the_board)
+    puts @display.score_board(@game.scores)
+    loop do
+      print @display.wrong_input2
+      yes = @game.play_again?(gets.chomp)
+      if yes == true
+        @board.reset_board
+        break
+      elsif yes == false
+        @game_over = true
+        break
+      else
+        print @display.wrong_input2(true)
+      end
+    end
+  end
+
+until @game_over
+
+
+    puts @display.print_board(@board.the_board)
+    print @display.ask_user(@board.the_player)
+
+    loop do
+      yes = @game.usr_input_validate?(gets.chomp)
+      @position = yes
+      break if yes.is_a? Integer
+
+      print @display.wrong_input
+    end
+
+    if @board.pos_valid?(@position)
+      @board.update_board(@position)
+      win_method if @board.won?
+      draw_method if @board.draw?
+    else
+      puts @display.position_taken
+    end
+    @board.change_player
+  end
--- lib/game.rb
+++ lib/game.rb
@@ -2,50 +2,14 @@ class Game
   attr_reader :scores
 
   def initialize
-    @board = Board.new
-    @display = Display.new
-    @game_over = false
     @scores = { 'X' => 0, 'O' => 0, 'draws' => 0 }
-    @postion = nil
   end
 
   def usr_input_validate?(input)
     if ('1'..'9').include? input
-      @position = input.to_i
-      true
+      return input.to_i
     else
       false
-    end
-  end
-
-  def start
-    until @game_over
-
-      puts @display.print_board(@board.the_board)
-      print @display.ask_user(@board.the_player)
-
-      loop do
-        yes = usr_input_validate?(gets.chomp)
-        break if yes == true
-
-        print @display.wrong_input
-      end
-
-      if @board.pos_valid?(@position)
-        @board.update_board(@position)
-        if @board.won?
-          puts @display.player_wins(@board.the_player)
-          @scores[@board.the_player] += 1
-          end_game_handler
-        elsif @board.draw?
-          @scores['draws'] += 1
-          puts @display.draw
-          end_game_handler
-        end
-        @board.change_player
-      else
-        puts @display.position_taken
-      end
     end
   end
 
@@ -59,23 +23,4 @@ class Game
     end
   end
 
-  private
-
-  def end_game_handler
-    @display.print_board(@board.the_board)
-    @display.score_board(@scores)
-    loop do
-      print @display.wrong_input2
-      yes = play_again?(gets.chomp)
-      if yes == true
-        @board.reset_board
-        break
-      elsif yes == false
-        @game_over = true
-        break
-      else
-        print @display.wrong_input2(true)
-      end
-    end
-  end
 end
